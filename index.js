21a603de429cc46db06d422a09014522158546cf
Jacob Madden <jacob.madden@connectsolutions.com>
2015-02-14 01:44:41 -0500

    init

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var Hapi = require('hapi');
 
-var server = new Hapi.Server(~~process.env.PORT || 8000, '0.0.0.0');
+var server = new Hapi.Server(~~process.env.PORT || 5000, '0.0.0.0');
 
 server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: function (request, reply) {
-       reply('hello world');
-    }
-});
+  method: 'GET',
+  path:'/hello',
+  handler: function (request, reply) {
+     reply('hello world');
+  }
+},
+{
+  method: 'GET',
+  path:'/',
+  handler: function (request, reply) {
+     reply('hello');
+  }
+}
+);
 
 server.start();
